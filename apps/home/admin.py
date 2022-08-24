bc7b7d9f20c92f799994c24280c7ca40f2aace8a
lolylynm <86237533+lolylynm@users.noreply.github.com>
2022-08-24 13:55:51 +0200

    samples now working

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -39,18 +39,16 @@ admin.site.register(Sample, Sample_Admin)
 # admin.site.register(Patient,PatientAdmin)
 
 # admin.site.register(Patient,AuthorAdmin)
-@admin.register(Patient)
-class TaskAdmin(admin.ModelAdmin):
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        formfield = super().formfield_for_foreignkey(db_field, request, **kwargs)
-        if db_field.name == "user":
-            formfield.label_from_instance = lambda obj: f'{obj.first_name} ({obj.last_name})'
-        return formfield
+# @admin.register(Patient)
+# class TaskAdmin(admin.ModelAdmin):
+#     def formfield_for_foreignkey(self, db_field, request, **kwargs):
+#         formfield = super().formfield_for_foreignkey(db_field, request, **kwargs)
+#         if db_field.name == "user":
+#             formfield.label_from_instance = lambda obj: f'{obj.first_name} ({obj.last_name})'
+#         return formfield
 
 class PatientAdmin(admin.ModelAdmin):
     list_display = ('name','id','study','age')
-
-
 admin.site.register(Patient,PatientAdmin)
 
 
